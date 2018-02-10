db3428cef70d51e189183084febce2079e8b235f
Freja <fkry@itu.dk>
2018-02-10 22:51:00 +0100

    Patrol

---
+++ Hello/Assets/PatrolAction.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CreateAssetMenu (menuName = "PluggableAI/State")]
+public class State : ScriptableObject
+{
+    public Action[] actions;
+    public Color sceneGizmoColor = Color.grey;
+
+    public void UpdateState(StateController controller)
+    {
+        DoActions(controller);
+    }
+
+    private void DoActions(StateController controller)
+    {
+        for (int i = 0; i < actions.Length; i++) {
+            actions[i].Act (controller);
+        }
+    }
+}
