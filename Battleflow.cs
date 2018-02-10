5d765e8baab8c1b47f39c019ef54c3b276ec9572
Speer-head <caspertaccopiv@gmail.com>
2018-02-10 18:06:15 +0100

    Heyo cotton eyed joe, where did you long time go. Where did you come from where did you go? where did you come from cotton eyed Joe!

---
+++ Battleflow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Battleflow : MonoBehaviour {
+
+	public static int whoseTurn = 1;
+	public Collider col;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (whoseTurn == 2) {
+			var cols = Physics.OverlapBox (col.bounds.center, col.bounds.extents, col.transform.rotation, LayerMask.GetMask ("Enemy"));
+			int alphabetium = 100000000;
+		}
+	}
+}
