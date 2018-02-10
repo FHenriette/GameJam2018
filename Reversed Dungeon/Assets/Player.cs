5f13791136ac4449a1f912347c22e292c2bb6d57
Cakeminator <31987858+Cakeminator@users.noreply.github.com>
2018-02-10 21:36:44 +0100

    yes
    Scripts for the gameObjects

---
+++ Reversed Dungeon/Assets/Player.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : CharacterStats {
+
+	//Stats of a character
+	public int startHealth = 100; //Starting Health
+	public int currentHealth; //Current Health, change this over the course of the game
+	public int startMana = 15; //Starting Mana
+	public int currentMana; //Current Mana, change this over the course of the game
+	public int startArmor = 0; //Starting Armor
+	public int currentArmor; //Current Armor, change this over the course of the game
+	public int startAttack = 10; //Starting damage
+	public int currentAttack; //Current damage, change this over the course of the game
+
+	//Action Points
+	public float startMP = 3; //MP = Movement Points
+	public float currentMP; //MP = Movement Points, change this over the course of the game
+	public float startAP = 1; //AP = Attack Points
+	public float currentAP; //AP = Attack Points, change this over the course of the game
+
+	//Turn
+	public int turn;
+
+	//Attacks
+	bool melee = true;
+	bool ranged = false;
+	bool spell = false;
+
+	//Deadsies
+	bool isDead = false;
+
+	// Use this for initialization
+	void Start () {
+
+		currentAttack = startAttack;
+		currentArmor = startArmor;
+		currentHealth = startHealth;
+		currentMana = startMana;
+		currentMP = startMP;
+		currentAP = startAP;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		PlayerGainExp ();
+		PlayerDeath ();
+
+		/*
+		public void takeDamage(int damage){
+
+			currentHealth =- (damage - currentArmor);
+
+		}
+
+		if(currentHealth <= 0 && !isDead){
+			Death();
+		}
+
+		public void Death(){
+
+		isDead = true;
+
+		playerMovement = false;
+		playerMelee = false;
+		playerRanged = false;
+	*/
+	}
+
+	public void PlayerAttack(){
+
+	}
+
+	public void takeDamage(){
+
+		Player.currentHealth =- (Enemy.currentAttack - Player.currentArmor);
+
+	}
+
+	public void PlayerDeath(){
+
+		if(Player.currentHealth <= 0 && !PlayerisDead){
+			PlayerisDead = true;
+
+			//playerMovement = false;
+			//playerMelee = false;
+			//playerRanged = false;
+			//save score
+			//stop the game
+			//display score on a screen
+		}
+	}
+}
---
+++ Reversed Dungeon/Assets/PlayerAbilities.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAbilities : Player {
+
+	public void PlayerGainExp(){ //This one is constant, so should be in update. Should also just stand alone
+		//outside of a void itself
+
+		if (enemy gameObject == Enemy.isDead) { //This obviously aint done
+			currentHealth += 10;
+			if (Enemy.Levels < Player.currentLevel) {
+				Player.currentExp += killExp;
+			} 
+			else if (Enemy.Levels == Player.currentLevel) {
+				Player.currentExp += killExp;
+			} 
+			else if (Enemy.Levels > Player.currentLevel) {
+				Player.currentExp += killExp;
+			}
+		}
+
+		if (Player.currentExp >= newLevelExp) {
+			playerLevelUp ();
+		}
+	}
+
+	public void PlayerAttacks (int damage){
+
+		damage = Player.currentAttack;
+
+		//These 3 specifies which attack you want toggled
+		if (Input.GetKeyDown ("1")) { 
+			print ("Melee attack is now toggled");
+			damage = Player.currentAttack;
+			Player.melee = true;
+			Player.ranged = false;
+			Player.spell = false;
+		}
+		if (Input.GetKeyDown ("2")) {
+			print ("Ranged attack is now toggled");
+			damage = *0.75;
+			melee = false;
+			ranged = true;
+			spell = false;
+		}
+		if (Input.GetKeyDown ("3")) {
+			print ("Spell is now toggled");
+			damage = *1.50f;
+			melee = false;
+			ranged = false;
+			spell = true;
+		}
+		if (Input.GetMouseButtonDown ("0" || "1" || "2")) {
+			print (BattleFlow.enemies [i] + " has been targeted");
+			//tags the enemy with left, right, middle mouse click.
+		}
+		if (Input.GetKeyDown ("enter")) {
+			print ("Turn Ending");
+			turn++; // 
+		}
+		//What happens when attacking
+		if (Input.GetKeyDown ("space")) {
+			if (Player.currentAP > 0) {
+				print ("I stab at thee");
+				if (melee = true) {
+					Attack ();
+					Player.currentAP = -1;
+				}
+				if (ranged = true) {
+					Attack ();
+					Player.currentAP = -1;
+				}
+				if (spell = true) {
+					Attack ();
+					Player.currentMana = -5;
+					Player.currentAP = -1;
+				}
+			}
+		}
+	}
+
+	public void PlayerGenes(){
+
+		if (Player.currentLevel == 2 || 4 || 6 || 8 || 10 || 12) {
+
+			int value;
+			Random geneLevel = value (1, 12);
+			if (value == 1 || 5 || 9) {
+				print ("Scaly Hide have been evolved");
+				Player.currentHealth += 30;
+				Player.currentArmor += 2;
+			}
+			if (value == 2 || 6 || 10) {
+				print ("Claws have been evolved");
+				Player.currentAttack += 10;
+				Player.currentHealth += 10;
+			}
+			if (value == 3 || 7 || 11) {
+				print ("Magic Aura have been evolved");
+				Player.currentHealth += 20;
+				Player.currentArmor += 2;
+				Player.currentMana += 10;
+			}
+			if(value == 4 || 8 || 12) {
+				print ("Shooting Spikes have been evolved");
+				Player.currentAttack += 5;
+				Player.currentArmor += 2;
+			}
+		}
+	}
+
+}
