61715a3bf32f4d3f37c6aa88cd30b0ace1027432
Svampex <adra@itu.dk>
2018-02-10 22:18:46 +0100

    new textures for walls and tiles

--- Reversed Dungeon/Assets/RoomGenerator.cs
+++ Reversed Dungeon/Assets/RoomGenerator.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class RoomGenerator : MonoBehaviour {
 	public GameObject tile;
 	public GameObject wall;
+	public List<Sprite> tileTextures;
+	public List<Texture> wallTextures;
 	HashSet<Room> generatedRooms = new HashSet<Room>();
 	// Use this for initialization
 	void Start () {
@@ -19,6 +21,7 @@ public class RoomGenerator : MonoBehaviour {
 				//rObject.transform.position = new Vector3 (r.x, r.y, 0);
 				for (int x = r.x; x <= r.x + r.width; x++) {
 					for (int y = r.y; y <= r.y + r.height; y++) {
+						GameObject obj;
 						if ((x == r.x || x == r.x + r.width) || (y == r.y || y == r.y + r.height)) {
 							bool door = false;
 							foreach (Door d in r.doors.Values) {
@@ -29,13 +32,21 @@ public class RoomGenerator : MonoBehaviour {
 									}
 								}
 							}
+
 							if (door) {
 								Debug.Log ("Door here!");
-								GameObject.Instantiate (tile, new Vector3 (x, y, 0.5f), Quaternion.identity).transform.SetParent(rObject.transform);	
+								obj = GameObject.Instantiate (tile, new Vector3 (x, y, 0.5f), Quaternion.identity);
+								obj.transform.SetParent (rObject.transform);	
+								obj.GetComponent<SpriteRenderer> ().sprite = tileTextures[Random.Range(0, tileTextures.Count)];
+							} else {
+								obj = GameObject.Instantiate (wall, new Vector3 (x, y, 0), Quaternion.Euler(90*Random.Range(0,3),-90,90));
+								obj.transform.SetParent (rObject.transform);
+								obj.GetComponentInChildren<MeshRenderer>().material.mainTexture = wallTextures[Random.Range(0, wallTextures.Count)];
 							}
-							else GameObject.Instantiate (wall, new Vector3 (x, y, 0), Quaternion.identity).transform.SetParent (rObject.transform);
 						} else {
-							GameObject.Instantiate (tile, new Vector3 (x, y, 0.5f), Quaternion.identity).transform.SetParent(rObject.transform);
+							obj = GameObject.Instantiate (tile, new Vector3 (x, y, 0.5f), Quaternion.identity);
+							obj.transform.SetParent(rObject.transform);
+							obj.GetComponent<SpriteRenderer>().sprite = tileTextures[Random.Range(0, tileTextures.Count)];
 						}
 					}
 				}
