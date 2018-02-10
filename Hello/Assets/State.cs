de5904263fc7851c0070457fd004c8db90c03ca1
Freja <fkry@itu.dk>
2018-02-10 22:27:48 +0100

    Hello

---
+++ Hello/Assets/State.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CreateAssetMenu (menuName = "PluggableAI/State")]
+public class State : ScriptableObject
+{
+    public Action[] actions;
+
+    public void UpdateState(StateController controller)
+    {
+        DoActions(controller);
+    }
+
+    private void DoActions(StateController controller)
+    {
+        for (int i = 0; i < actions.Length; i++) {
+            actions[i].Act (controller);
+        }
+    }
+}
