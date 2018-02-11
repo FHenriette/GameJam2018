c19e1885af57ccf5890909e975cda865275ffd5d
Svampex <adra@itu.dk>
2018-02-11 09:55:14 +0100

    done something, dont remember

---
+++ Reversed Dungeon/Assets/UIControl.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class UIControl : MonoBehaviour {
+	public Text Level, Health, Mana, MovementPoints, Attack, Defense, AttackPoints;
+	public Image WinPanel, GameOverPanel;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
